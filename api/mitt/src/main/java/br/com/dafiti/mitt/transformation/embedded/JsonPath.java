2a5416fe4f54361aba5b6122333eb5133c02a4f1
Helio Leal <helio.leal@dafiti.com.br>
2020-04-27 11:50:11 -0300

    Inclusão das novas transformações para o mitt.

---
+++ api/mitt/src/main/java/br/com/dafiti/mitt/transformation/embedded/JsonPath.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020 Dafiti Group
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package br.com.dafiti.mitt.transformation.embedded;
+
+import br.com.dafiti.mitt.transformation.Parser;
+import br.com.dafiti.mitt.transformation.Transformable;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Fernando Saga
+ * @author Valdiney V GOMES
+ */
+public class JsonPath implements Transformable {
+
+    private final String field;
+    private final String path;
+
+    public JsonPath(String field, String path) {
+        this.field = field;
+        this.path = path;
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public String getValue(
+            Parser parser,
+            List<Object> record) {
+
+        String json = null;
+        String value = "";
+
+        try {
+            //Evaluates the field to retrieve a json. 
+            json = String.valueOf(parser.evaluate(record, field));
+
+            if (json != null) {
+                //Extracts the desired value from json. 
+                value = com.jayway.jsonpath.JsonPath
+                        .parse(json)
+                        .read(this.path, String.class);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(JsonPath.class.getName()).log(Level.SEVERE, "Error parsing json {0}", json);
+        }
+
+        return value;
+    }
+}
---
+++ api/mitt/src/main/java/br/com/dafiti/mitt/transformation/embedded/SplitPart.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2020 Dafiti Group
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+package br.com.dafiti.mitt.transformation.embedded;
+
+import br.com.dafiti.mitt.transformation.Parser;
+import br.com.dafiti.mitt.transformation.Transformable;
+import com.google.common.base.Splitter;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Returns only a part of a field value based on a given delimiter
+ * 
+ * @author Fernando Saga
+ * @author Helio Leal
+ */
+public class SplitPart implements Transformable {
+
+    private final String field;
+    private final String delimiter;
+    private final String position;
+
+    public SplitPart(String field, String delimiter, String position) {
+        this.field = field;
+        this.delimiter = delimiter;
+        this.position = position;
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public String getValue(
+            Parser parser,
+            List<Object> record) {
+        String value = "";
+
+        if (StringUtils.isNumeric(this.position)) {
+            List<String> list = Splitter
+                    .on(this.delimiter)
+                    .splitToList((String) parser.evaluate(record, field));
+
+            //Identifies if array out of bounds won't happen.
+            if (Integer.valueOf(this.position) < list.size()) {
+                value = list.get(Integer.valueOf(this.position));
+            }
+        }
+
+        return value;
+    }
+}
