90108c4ac51cf929e74970e86dc299dbdf17ebec
Helio Leal <helio.leal@dafiti.com.br>
2020-07-08 17:53:29 -0300

    Extrator do One Signal

--- api/one-signal/src/main/java/br/com/dafiti/one/signal/OneSignal.java
+++ api/one-signal/src/main/java/br/com/dafiti/one/signal/OneSignal.java
@@ -29,16 +29,22 @@ import br.com.dafiti.mitt.exception.DuplicateEntityException;
 import br.com.dafiti.mitt.transformation.embedded.Concat;
 import br.com.dafiti.mitt.transformation.embedded.Now;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -95,7 +101,7 @@ public class OneSignal {
             //Identifies original fields.
             List<String> fields = mitt.getConfiguration().getOriginalFieldsName();
 
-            Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "Retrieving data from URL: {0}", new Object[]{cli.getParameter("endpoint")});
+            Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "Send {0} request for endpoint: {1}", new Object[]{cli.getParameter("method"), cli.getParameter("endpoint")});
 
             //Connect to API.
             HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(cli.getParameter("endpoint")).openConnection();
@@ -108,15 +114,64 @@ public class OneSignal {
                     new InputStreamReader(httpURLConnection.getInputStream()))) {
                 String response;
 
-                //Get service list from API.
+                //Get endpoint response from API.
                 while ((response = bufferedReader.readLine()) != null) {
                     String url = (String) ((JSONObject) new JSONParser()
                             .parse(response))
-                            .get("csv_file_url");                    
+                            .get("csv_file_url");
 
-                    Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "{0} {1} found ", new Object[]{jsonObject.size(), cli.getParameter("service")});
+                    //Identify if file is ready
+                    boolean ready = false;
 
-                    
+                    while (!ready) {
+                        HttpURLConnection httpConnection = (HttpURLConnection) new URL(url).openConnection();
+                        int responseCode = httpConnection.getResponseCode();
+
+                        //Identifies if file is ready to download.
+                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                            Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "Downloading file from URL: {0}", new Object[]{url});
+
+                            httpConnection.disconnect();
+                            ready = true;
+
+                            //Defines the output path for temp files.
+                            Path outputPath = Files.createTempDirectory("ftp_");
+
+                            //Download file from URL
+                            FileUtils.copyURLToFile(
+                                    new URL(url),
+                                    new File(outputPath + "/csv_export.csv.gz")
+                            );
+
+                            Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "Writing output file to: {0}", cli.getParameter("output"));
+
+                            //Write to the output.
+                            mitt.getReaderSettings().setDelimiter(cli.getParameter("delimiter").charAt(0));
+                            mitt.getReaderSettings().setEncode(cli.getParameter("encode"));
+                            mitt.write(outputPath.toFile(), "*");
+                        } else {
+                            //Response 403 means that file is not ready.
+                            if (responseCode == HttpURLConnection.HTTP_FORBIDDEN) {
+                                Logger.getLogger(OneSignal.class.getName()).log(Level.INFO, "File {0} is not ready to download, error: {1}", new Object[]{url, responseCode});
+
+                                //Identify if has sleep time until next API call.
+                                if (cli.getParameterAsInteger("sleep") > 0) {
+                                    try {
+                                        Logger.getLogger(OneSignal.class.getName())
+                                                .log(Level.INFO, "Sleeping {0} seconds until next try", cli.getParameterAsInteger("sleep"));
+
+                                        Thread.sleep(Long.valueOf(cli.getParameterAsInteger("sleep") * 1000));
+                                    } catch (InterruptedException ex) {
+                                        Logger.getLogger(OneSignal.class.getName()).log(Level.SEVERE, null, ex);
+                                    }
+                                }
+
+                            } else {
+                                ready = true;
+                                Logger.getLogger(OneSignal.class.getName()).log(Level.SEVERE, "GLOVE - One Signal fail: ", responseCode);
+                            }
+                        }
+                    }
                 }
             }
             httpURLConnection.disconnect();
