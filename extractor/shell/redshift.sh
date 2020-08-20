8b1bd7c5fac17e6438573347a778a350ade6ad48
Valdiney Gomes <valdiney.gomes@dafiti.com.br>
2020-08-20 15:41:16 -0300

    Melhoria no processo de remoção de cabeçalhos de arquivos grandes processados pelo Glove

--- extractor/shell/redshift.sh
+++ extractor/shell/redshift.sh
@@ -287,19 +287,18 @@ if [ ${QUEUE_FILE_COUNT} -gt 0 ]; then
 	
     table_check
 
-	# Identifica se é uma named query.
-    if [ ${MODULE} == "query" ] || [ ${MODULE} == "file" ]; then
-		# Remove o header do csv intermediário.
-		echo "Removing header from ${RAWFILE_QUEUE_FILE}!"
-		tail -n +2 ${RAWFILE_QUEUE_FILE} > ${RAWFILE_QUEUE_FILE}.tmp
-		mv -f ${RAWFILE_QUEUE_FILE}.tmp ${RAWFILE_QUEUE_FILE};
-    fi
-
 	# Particiona o arquivo intermediário.
 	echo "Splitting csv file!"
 	split -l ${PARTITION_LENGTH} -a 4 --numeric-suffixes=1 --additional-suffix=.csv ${RAWFILE_QUEUE_FILE} ${DATA_FILE}_
 	error_check
 	
+   	# Remove o header do csv intermediário.
+    if [ ${MODULE} == "query" ] || [ ${MODULE} == "file" ]; then
+    	echo "Removing header from ${RAWFILE_QUEUE_FILE}!"
+    	tail -n +2 ${DATA_FILE}_0001.csv > ${DATA_FILE}_0001.tmp
+    	mv -f ${DATA_FILE}_0001.tmp ${DATA_FILE}_0001.csv;
+    fi
+
 	# Remove o arquivo original.
 	echo "Removing file ${RAWFILE_QUEUE_FILE}!"
 	rm -f ${RAWFILE_QUEUE_FILE}
--- extractor/shell/spectrum.sh
+++ extractor/shell/spectrum.sh
@@ -345,6 +345,13 @@ full_load(){
 		split -l ${PARTITION_LENGTH} -a 4 --numeric-suffixes=1 --additional-suffix=.csv ${RAWFILE_QUEUE_FILE} ${DATA_FILE}_
 		error_check
 
+   		# Remove o header do csv intermediário.
+    	if [ ${MODULE} == "query" ]; then
+        	echo "Removing header from ${RAWFILE_QUEUE_FILE}!"
+        	tail -n +2 ${DATA_FILE}_0001.csv > ${DATA_FILE}_0001.tmp
+        	mv -f ${DATA_FILE}_0001.tmp ${DATA_FILE}_0001.csv;
+    	fi
+
 		# Remove o arquivo original.
 		echo "Removing file ${RAWFILE_QUEUE_FILE}!"
 		rm -f ${RAWFILE_QUEUE_FILE}
@@ -666,13 +673,6 @@ if [ ${QUEUE_FILE_COUNT} -gt 0 ]; then
             if [ "${#DELTA_FIELD_IN_METADATA}" -gt "0" ]; then
 			    delta_load
 		    else
-                # Remove o header do csv intermediário.
-                if [ ${MODULE} == "query" ]; then
-                    echo "Removing header from ${RAWFILE_QUEUE_FILE}!"
-                    tail -n +2 ${RAWFILE_QUEUE_FILE} > ${RAWFILE_QUEUE_FILE}.tmp
-                    mv -f ${RAWFILE_QUEUE_FILE}.tmp ${RAWFILE_QUEUE_FILE};
-                fi
-
 			    full_load
 		    fi
 	    fi
