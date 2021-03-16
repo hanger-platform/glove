eb06e1fb734c05cb1b020b216fc8711df7cbd5f9
Valdiney V GOMES <valdiney.gomes@dafiti.com.br>
2021-03-16 19:05:26 -0300

    Correção no processo de compactação de arquivo

--- extractor/shell/spectrum.sh
+++ extractor/shell/spectrum.sh
@@ -651,14 +651,13 @@ if [ ${QUEUE_FILE_COUNT} -gt 0 ]; then
 
 				# Identifica se deve compactar o arquivo a ser exportado.
 				if [ ${EXPORT_TYPE} == "gz" ]; then
+					pigz -k ${RAWFILE_QUEUE_PATH}*
+
 					# Envia o arquivo compactado para o bucket de destino.
 					if [ "${#PARTITION_FIELD}" -gt "0" ]; then
-						pigz -r -k ${RAWFILE_QUEUE_PATH}*
-
 						aws s3 rm ${EXPORT_BUCKET} --profile ${EXPORT_PROFILE} --recursive
 						aws s3 cp ${RAWFILE_QUEUE_PATH} ${EXPORT_BUCKET} --profile ${EXPORT_PROFILE} --recursive --exclude "${DATA_FILE}*" --exclude "*.csv" --only-show-errors --acl bucket-owner-full-control
 					else
-						pigz -k ${RAWFILE_QUEUE_PATH}*
 						aws s3 cp ${RAWFILE_QUEUE_FILE}.gz ${EXPORT_BUCKET} --profile ${EXPORT_PROFILE} --only-show-errors --acl bucket-owner-full-control
 					fi
 					error_check
