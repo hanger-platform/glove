aa27dc68433c310bd0167876336b5c908a830702
Helio Leal <helio.leal@dafiti.com.br>
2021-06-01 18:24:55 -0300

    Alterações gerais no extrator SAPJCO3

--- api/sapjco3/src/main/java/br/com/dafiti/sapjco3/Sapjco3.java
+++ api/sapjco3/src/main/java/br/com/dafiti/sapjco3/Sapjco3.java
@@ -30,6 +30,7 @@ import br.com.dafiti.mitt.transformation.embedded.Concat;
 import br.com.dafiti.mitt.transformation.embedded.Now;
 import com.sap.conn.jco.JCoDestination;
 import com.sap.conn.jco.JCoDestinationManager;
+import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoTable;
 import com.sap.conn.jco.ext.DestinationDataEventListener;
@@ -41,7 +42,6 @@ import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -70,12 +70,14 @@ public class Sapjco3 {
                     .addParameter("c", "credentials", "Credentials file", "", true, false)
                     .addParameter("o", "output", "Output file", "", true, false)
                     .addParameter("f", "function", "RFC function name", "", true, false)
+                    .addParameter("t", "table", "table name", "", true, false)
                     .addParameter("fi", "field", "(Optional)  Fields to be retrieved, if not passed, try to get all of them.", "")
-                    .addParameter("i", "import", "(Optional)  Json Object - Function importation parameters.", "")
-                    .addParameter("t", "tables", "(Optional)  Json Array - Function tables parameters.", "")
+                    .addParameter("i", "where", "(Optional)  Where condition.", "")
                     .addParameter("a", "partition", "(Optional)  Partition, divided by + if has more than one field", "")
                     .addParameter("k", "key", "(Optional) Unique key, divided by + if has more than one field", "")
-                    .addParameter("d", "input_delimiter", "(Optional) SAPJCO3 function resultset return delimiter; '|' as default", "\\|");
+                    .addParameter("d", "input_delimiter", "(Optional) SAPJCO3 function resultset return delimiter; '|' as default", "|")
+                    .addParameter("r", "row_count", "(Optional) how many records will return at once; '0' as default and means return everything", "0")
+                    .addParameter("d", "delimiter", "(Optional) SAPJCO3 function resultset return delimiter; '|' as default", "\\|");
 
             //Reads the command line interface. 
             CommandLineInterface cli = mitt.getCommandLineInterface(args);
@@ -124,77 +126,89 @@ public class Sapjco3 {
             }
 
             JCoDestination destination = JCoDestinationManager.getDestination("ABAP_AS");
-            JCoFunction function = destination.getRepository().getFunction(cli.getParameter("function"));
 
-            //Retrieves function importation parameters.
-            if (cli.getParameter("import") != null) {
-                JSONObject importParameters = (JSONObject) parser.parse(cli.getParameter("import"));
+            int rowCount = cli.getParameterAsInteger("row_count");
+            int rowSkips = 0;
+            int numRows = ZRFCGetTableCount(destination, cli);
 
-                if (importParameters != null && !importParameters.isEmpty()) {
-                    for (Object key : importParameters.keySet()) {
-                        function.getImportParameterList().setValue((String) key, importParameters.get(key));
+            LOG.log(Level.INFO, "Table {0} has {1} records [WHERE CONDITION: {2}].", new Object[]{cli.getParameter("table"), numRows, cli.getParameter("where")});
+
+            //Retrieves data based on row count.
+            do {
+                JCoFunction function = destination.getRepository().getFunction(cli.getParameter("function"));
+
+                //Identifies if function was found on SAP Server.
+                if (function != null) {
+                    //Defines RFC import parameters.
+                    function.getImportParameterList().setValue("QUERY_TABLE", cli.getParameter("table"));
+                    function.getImportParameterList().setValue("DELIMITER", cli.getParameter("input_delimiter"));
+
+                    //Identifies how many rows will retrieve at once.
+                    if (rowCount > 0) {
+                        function.getImportParameterList().setValue("ROWCOUNT", rowCount);
+                        function.getImportParameterList().setValue("ROWSKIPS", rowSkips);
                     }
-                }
-            }
 
-            //Retrieves function tables parameters.
-            if (cli.getParameter("tables") != null) {
-                JSONArray tablesParameters = (JSONArray) parser.parse(cli.getParameter("tables"));
+                    //Defines RFC tables parameters.
+                    if (cli.getParameter("where") != null) {
+                        JCoTable options = function.getTableParameterList().getTable("OPTIONS");
+                        options.appendRow();
+                        options.setValue("TEXT", cli.getParameter("where"));
+                    }
 
-                if (tablesParameters != null && !tablesParameters.isEmpty()) {
-                    for (Object tableParameter : tablesParameters) {
-                        JSONObject parameter = (JSONObject) tableParameter;
+                    if (cli.getParameter("field") != null) {
+                        JCoTable options = function.getTableParameterList().getTable("FIELDS");
 
-                        JCoTable options = function.getTableParameterList().getTable(parameter.get("TABLE").toString());
-                        JSONArray parameterValues = (JSONArray) parameter.get("VALUES");
+                        for (String field : cli.getParameterAsList("field", "\\+")) {
+                            options.appendRow();
+                            options.setValue("FIELDNAME", field.toUpperCase());
+                        }
+                    }
 
-                        for (Object values : parameterValues) {
-                            JSONObject value = (JSONObject) values;
+                    //Executes function.
+                    function.execute(destination);
 
-                            for (Object key : value.keySet()) {
-                                options.appendRow();
-                                options.setValue((String) key, value.get(key));
+                    //If no fields were informed, try to automatically get fields.
+                    if (!cli.hasParameter("field")) {
+                        final JCoTable fields = function.getTableParameterList().getTable("FIELDS");
+
+                        //Fields are only set once.
+                        boolean hasFields = configuration.getOriginalField().size() > 0;
+
+                        if (!hasFields) {
+                            for (int i = 0; i < fields.getNumRows(); i++) {
+                                fields.setRow(i);
+                                configuration.addField(fields.getString("FIELDNAME"));
                             }
                         }
                     }
-                }
-            }
 
-            //Identifies if function was found on SAP Server.
-            if (function != null) {
-                //Execute ABAP function.
-                function.execute(destination);
+                    final JCoTable rows = function.getTableParameterList().getTable("DATA");
 
-                //If no fields were informed, try to automatically get fields.
-                if (!cli.hasParameter("field")) {
-                    final JCoTable fields = function.getTableParameterList().getTable("FIELDS");
+                    LOG.log(Level.INFO, "This request returned {0} rows [ROWCOUNT: {1}, ROWSKIPS: {2}].", new Object[]{rows.getNumRows(), rowCount, rowSkips});
 
-                    for (int i = 0; i < fields.getNumRows(); i++) {
-                        fields.setRow(i);
-                        configuration.addField(fields.getString("FIELDNAME"));
-                    }
-                }
+                    for (int i = 0; i < rows.getNumRows(); i++) {
+                        List record = new ArrayList();
 
-                final JCoTable rows = function.getTableParameterList().getTable("DATA");
+                        rows.setRow(i);
+                        String row = rows.getString("WA") + "\n";
+                        String[] values = row.split(cli.getParameter("delimiter"));
 
-                LOG.info("This request returned a resultset with "+rows.getNumRows()+" rows.");
+                        for (int y = 0; y < values.length; y++) {
+                            record.add(values[y].trim());
+                        }
 
-                for (int i = 0; i < rows.getNumRows(); i++) {
-                    List record = new ArrayList();
-
-                    rows.setRow(i);
-                    String row = rows.getString("WA") + "\n";
-                    String[] values = row.split(cli.getParameter("input_delimiter"));
-
-                    for (int y = 0; y < values.length; y++) {
-                        record.add(values[y].trim());
+                        mitt.write(record);
                     }
 
-                    mitt.write(record);
+                    //Increments lines to skip.
+                    rowSkips += rowCount;
+
+                } else {
+                    throw new Exception("Function not found in SAP Server");
                 }
-            } else {
-                throw new Exception("Function not found in SAP Server");
-            }
+                //Identifies if row count is greater than 0 and if rowskips reached limit.
+            } while (rowCount > 0 && rowSkips < numRows);
 
         } catch (Exception ex) {
             LOG.log(Level.SEVERE, "GLOVE - SAPJCO3 extractor fail: ", ex);
@@ -239,5 +253,27 @@ public class Sapjco3 {
                 }
             }
         }
+    }
+
+    /**
+     *
+     * @param destination JCoDestination
+     * @param cli CommandLineInterface
+     * @return Number of entries of a table.
+     * @throws JCoException
+     */
+    static private int ZRFCGetTableCount(JCoDestination destination, CommandLineInterface cli) throws JCoException {
+        JCoFunction function = destination.getRepository().getFunction("ZRFC_GET_TABLE_COUNT");
+        function.getImportParameterList().setValue("TABLE_NAME", cli.getParameter("table"));
+
+        //Defines RFC tables parameters.
+        if (cli.getParameter("where") != null) {
+            function.getImportParameterList().setValue("WHERE_CONDITION", cli.getParameter("where"));
+        }
+
+        //Executes function.
+        function.execute(destination);
+
+        return function.getExportParameterList().getInt("COUNT");
     }
 }
