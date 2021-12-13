1720e1cc76ec3671a624e95b2d7ab3c862110476
Helio Leal <helio.leal@dafiti.com.br>
2021-12-13 18:04:06 -0300

    Chamada para trazer os dados da API.

--- api/sap-hana-cloud-api/src/main/java/br/com/dafiti/sap/hana/cloud/api/SapHanaCloudApi.java
+++ api/sap-hana-cloud-api/src/main/java/br/com/dafiti/sap/hana/cloud/api/SapHanaCloudApi.java
@@ -29,6 +29,7 @@ import br.com.dafiti.mitt.model.Configuration;
 import br.com.dafiti.mitt.transformation.embedded.Concat;
 import br.com.dafiti.mitt.transformation.embedded.Now;
 import java.io.FileReader;
+import java.net.URLEncoder;
 import java.util.Base64;
 import java.util.List;
 import java.util.logging.Level;
@@ -41,6 +42,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  *
@@ -50,7 +52,7 @@ public class SapHanaCloudApi {
 
     private static final Logger LOG = Logger.getLogger(SapHanaCloudApi.class.getName());
     private static final String SAP_HANA_CLOUD_API_TOKEN_URL = "https://hdbprd.authentication.us10.hana.ondemand.com/oauth/token";
-    private static final String SAP_HANA_CLOUD_API_ENDPOINT = "https://gfg-monitor-prd.cfapps.us10.hana.ondemand.com/";
+    private static final String SAP_HANA_CLOUD_API_ENDPOINT = "https://gfg-monitor-prd.cfapps.us10.hana.ondemand.com/monitor/";
 
     /**
      *
@@ -58,6 +60,8 @@ public class SapHanaCloudApi {
      */
     public static void main(String[] args) {
         LOG.info("GLOVE - SAPBTP API extractor started");
+
+        JSONObject parameters = null;
 
         //Define the mitt.
         Mitt mitt = new Mitt();
@@ -68,9 +72,9 @@ public class SapHanaCloudApi {
                     .addParameter("c", "credentials", "Credentials file", "", true, false)
                     .addParameter("o", "output", "Output file", "", true, false)
                     .addParameter("f", "field", "Fields to be retrieved from an endpoint", "", true, false)
-                    //.addParameter("e", "endpoint", "Endpoint name", "", true, false)
-                    //.addParameter("p", "parameters", "(Optional) Endpoint parameters", "", true, true)
-                    //.addParameter("b", "object", "(Optional) Json object", "", true, true)
+                    .addParameter("e", "endpoint", "Endpoint name", "", true, false)
+                    .addParameter("p", "parameters", "(Optional) Endpoint parameters", "", true, true)
+                    .addParameter("b", "object", "(Optional) Json object", "", true, true)
                     //.addParameter("g", "paginate", "(Optional) Identifies if the endpoint has pagination", false)
                     .addParameter("a", "partition", "(Optional)  Partition, divided by + if has more than one field", "")
                     .addParameter("k", "key", "(Optional) Unique key, divided by + if has more than one field", "");
@@ -108,33 +112,110 @@ public class SapHanaCloudApi {
 
             //Connect to the API. 
             try (CloseableHttpClient client = HttpClients.createDefault()) {
+                //Retrieves API Token.
                 HttpGet httpGet = new HttpGet(SAP_HANA_CLOUD_API_TOKEN_URL);
 
                 //Sets default URI parameters. 
-                URIBuilder uriBuilder = new URIBuilder(httpGet.getURI())
+                URIBuilder tokenUriBuilder = new URIBuilder(httpGet.getURI())
                         .addParameter("grant_type", "client_credentials")
                         .addParameter("response_type", "token");
 
                 //Sets URI parameters. 
-                httpGet.setURI(uriBuilder.build());
+                httpGet.setURI(tokenUriBuilder.build());
 
+                //Encode username and password to make api call.
                 String encoding = Base64.getEncoder().encodeToString((username + ":" + password).getBytes("UTF-8"));
 
                 //Sets Headers
                 httpGet.addHeader("Authorization", "Basic " + encoding);
 
                 //Executes a request. 
-                CloseableHttpResponse response = client.execute(httpGet);
+                CloseableHttpResponse closeableHttpResponse = client.execute(httpGet);
 
                 //Gets a reponse entity. 
-                String entity = EntityUtils.toString(response.getEntity(), "UTF-8");
+                String tokenEntity = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
 
-                if (!entity.isEmpty()) {
+                //Identifies if token was received.
+                if (!tokenEntity.isEmpty()) {
+                    JSONObject tokenJson = (JSONObject) new JSONParser().parse(tokenEntity);
 
-                    System.out.println("entity: " + entity);
+                    if (!tokenJson.isEmpty()) {
+                        LOG.log(Level.INFO, "Token was received successfully");
+
+                        //Identifies endpoint parameters. 
+                        String endpointParameter = cli.getParameter("parameters");
+
+                        if (endpointParameter != null && !endpointParameter.isEmpty()) {
+                            try {
+                                parameters = (JSONObject) parser.parse("{\"$filter\":\"DT_HORA_INICIO_EXECUCAO ge 2021-12-13T16:17:00Z\"}");
+                            } catch (ParseException ex) {
+                                LOG.log(Level.INFO, "Fail parsing endpoint parameters: {0}", endpointParameter);
+                            }
+                        }
+
+                        //Retrieves endpoint Token.
+                        HttpGet endPoint = new HttpGet(SAP_HANA_CLOUD_API_ENDPOINT + URLEncoder.encode(cli.getParameter("endpoint")));
+
+                        //Sets default URI parameters. 
+                        URIBuilder uriBuilder = new URIBuilder(endPoint.getURI());
+
+                        //Sets endpoint URI parameters. 
+                        if (parameters != null && !parameters.isEmpty()) {
+                            for (Object k : parameters.keySet()) {
+                                uriBuilder.addParameter((String) k, (String) parameters.get(k));
+                            }
+                        }
+
+                        //Sets URI parameters. 
+                        httpGet.setURI(uriBuilder.build());
+
+                        //Set Headers.
+                        endPoint.addHeader("Authorization", "Bearer " + tokenJson.get("access_token").toString());
+
+                        //Executes a request. 
+                        CloseableHttpResponse responseHttp = client.execute(endPoint);
+
+                        //Gets a reponse entity. 
+                        String entity = EntityUtils.toString(responseHttp.getEntity(), "UTF-8");
+
+                        //Identifies if there are payload to process. 
+                        if (!entity.isEmpty()) {
+                            JSONObject json = (JSONObject) new JSONParser().parse(entity);
+
+                            //Identifies the response status code.
+                            switch ((int) responseHttp.getStatusLine().getStatusCode()) {
+                                case 200 /*OK*/:
+                                    Object object;
+
+                                    //Identifies which object should be picked up from the payload.
+                                    if (cli.getParameter("object") == null || cli.getParameter("object").isEmpty()) {
+                                        object = json.get("value");
+                                    } else {
+                                        if ("*".equals(cli.getParameter("object"))) {
+                                            object = json;
+                                        } else {
+                                            object = json.get(cli.getParameter("object"));
+                                        }
+                                    }
+
+                                    break;
+                                case 400 /*BAD REQUEST*/:
+                                    LOG.log(Level.INFO, "Bad Request");
+                                    break;
+                                case 403 /*FORBIDDEN*/:
+                                    LOG.log(Level.INFO, "Forbidden");
+                                    break;
+                                default:
+                                    throw new Exception("HTTP Exception " + responseHttp.getStatusLine().getStatusCode());
+                            }
+
+                        } else {
+                            throw new Exception("Empty response for data request " + httpGet.getURI());
+                        }
+                    }
 
                 } else {
-                    throw new Exception("Empty response entity for request " + httpGet.getURI());
+                    throw new Exception("Empty response for token request " + httpGet.getURI());
                 }
             }
         } catch (Exception ex) {
