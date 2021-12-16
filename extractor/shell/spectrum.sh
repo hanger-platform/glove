a94e6cb456cd2143767855b5e2ff8b06146f6c7a
Helio Leal <helio.leal@dafiti.com.br>
2021-12-16 19:42:38 -0300

    Adicionado link de presign para ser enviado por e-mail.

--- extractor/shell/spectrum.sh
+++ extractor/shell/spectrum.sh
@@ -762,7 +762,7 @@ if [ ${QUEUE_FILE_COUNT} -gt 0 ]; then
 					echo 'Presign createad expires in 604800 seconds:'
 					echo $LINK_PRESIGN
 
-					echo 'Arquivo foi exportado e está disponível para download em: ${LINK_PRESIGN}' | mutt -s 'EXPORT de planilha' ${EXPORT_SHEETS_RECIPIENTS}
+					echo "Arquivo foi exportado e está disponível para download em: ${LINK_PRESIGN}" | mutt -s 'EXPORT de planilha' ${EXPORT_SHEETS_RECIPIENTS}
 					
 					### TODO
 					###	Efetuar as limpezas abaixo
